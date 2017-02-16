13f2d30289e0aafcf9da28a2c08a361b721b4691
pnganga05 <pnganga05@gmail.com>
2017-02-16 12:46:43 +0300

    Created more pages

--- js/styles.js
+++ js/styles.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-    
+    $("#navtext").mouseenter(function() {
+        $(this).css("color", "grey");
+
+    });
+    $("#navtext").mouseleave(function() {
+        $(this).css("color", "white");
+
+    });
+      $("#navtext2").mouseenter(function() {
+        $(this).css("color", "grey");
+
+    });
+    $("#navtext2").mouseleave(function() {
+        $(this).css("color", "white");
+
+    });
+      $("#navtext3").mouseenter(function() {
+        $(this).css("color", "grey");
+
+    });
+    $("#navtext3").mouseleave(function() {
+        $(this).css("color", "white");
+
+    });
 });
